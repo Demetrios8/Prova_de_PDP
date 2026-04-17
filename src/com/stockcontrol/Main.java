184958be665e9d43bf4059fd484b925777a50e56
Demetrios Alejandro <demetriosalejandro1@gmail.com>
2026-04-17 20:03:39 -0300

    Update Main.java

--- src/com/stockcontrol/Main.java
+++ src/com/stockcontrol/Main.java
@@ -6,7 +6,6 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // Leitura dos dados do produto
         System.out.print("Digite o nome do produto: ");
         String name = sc.nextLine();
 
@@ -22,20 +21,19 @@ public class Main {
         // Criação do objeto Produto
         Product p = new Product(name, price, initialQty);
 
-        // Exibição das informações antes da venda
+     
         System.out.println("\nAntes da venda:");
         System.out.println("Produto: " + p.getName());
         System.out.println("Preço: " + p.getPrice());
         System.out.println("Quantidade em estoque: " + p.getQuantity());
 
-        // Processamento da venda
         if (soldQty > p.getQuantity()) {
             System.out.println("Estoque insuficiente para realizar a venda.");
         } else {
             p.setQuantity(p.getQuantity() - soldQty);
         }
 
-        // Exibição das informações depois da venda
+      
         System.out.println("\nDepois da venda:");
         System.out.println("Produto: " + p.getName());
         System.out.println("Preço: " + p.getPrice());
